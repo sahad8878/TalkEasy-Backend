5c65bda03f5e3fe65d66462d78ea14aaf7968a42
sahad <msahadpp@gmail.com>
2023-08-11 15:05:29 +0530

    socket.io implemented

--- server.js
+++ server.js
@@ -1,25 +1,66 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
+import messagesRoutes from "./routes/messagesRoutes.js";
+dotenv.config();
 
-import  dotenv from 'dotenv'
-import express from 'express'
-import cors from 'cors'
-import userRoutes from './routes/userRoutes.js'
-import chatRoutes from './routes/chatRoutes.js'
-import messagesRoutes from './routes/messagesRoutes.js'
-dotenv.config()
+import "./config/connectDB.js";
+import { errorHandler, notFound } from "./middlewares/errorMiddlwares.js";
+const app = express();
+app.use(express.json());
 
-import  './config/connectDB.js'
-import { errorHandler, notFound } from './middlewares/errorMiddlwares.js'
-const app = express()
-app.use(express.json())
+app.use(cors({}));
 
-app.use(cors({}))
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/messages", messagesRoutes);
 
-app.use("/api/user",userRoutes)
-app.use("/api/chat",chatRoutes)
-app.use("/api/messages",messagesRoutes)
+app.use(notFound);
+app.use(errorHandler);
 
-app.use(notFound)
-app.use(errorHandler)
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => console.log(`listening on port ${port}`));
 
-const port = process.env.PORT || 5000
-app.listen(port,()=>console.log(`listening on port ${port}`))
+import { Server } from "socket.io";
+
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("user joined room " + room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new messages", (newMessageReceived) => {
+    var chat = newMessageReceived.chat;
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageReceived.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageReceived);
+    });
+  });
+
+  socket.off("setup" , () => {
+    console.log("User disconnected");
+    socket.leave(userData._id)
+  })
+});
