41481078ea01a48f45cb210d4c82e9a1c57ebc23
sahad <msahadpp@gmail.com>
2023-08-15 12:04:58 +0530

    deployed frontend origin added

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ import { Server } from "socket.io";
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://talkeasy.netlify.app/",
   },
 });
 
